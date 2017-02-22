1d735aeea40dde4f6ec1ece4e2747105d0b9f5ae
alberts07 <aual4352@colorado.edu>
2017-02-22 13:01:34 -0700

    Update GPS_Rec.py

--- GPS_Rec.py
+++ GPS_Rec.py
@@ -3,10 +3,12 @@ import GPS_runner
 import numpy
 
 # ********************************************************
-myissue=GPS_runner.runner()
 # This code was written Feb 6th 2017 by Austin Alberts
 # it is written for python 2 as the xlwt will not cooperate with python3
 
+myissue=GPS_runner.runner()
+
+#Early Dev with choice coming from command line
 #if(len(sys.argv) == 2 ):
 #	choice=sys.argv[1]
 #Call subroutine with choice and let it do all this
