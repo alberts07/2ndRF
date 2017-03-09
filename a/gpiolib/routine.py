33edadcafdea34d9a0ded2ce6403abacc6bbc031
jayhuang520 <kingsleyzhijiehuang@gmail.com>
2017-03-09 03:09:53 +0000

    Added 6 more gpio pins

--- a/gpiolib/routine.py
+++ a/gpiolib/routine.py
@@ -1,5 +1,8 @@
 import wiringPi
 
+#This is the website for the Physical Pin out for Odroid XU4
+#http://odroid.com/dokuwiki/doku.php?id=en:xu3_hardware_gpio
+
 #Define 4 GPIO pins
 
 ON     = 1
@@ -9,6 +12,12 @@ GPIO0  = 0 	# Physical pin 5
 GPIO1  = 15	# Physical pin 8
 GPIO2  = 21	# Physical pin 24
 GPIO3  = 11	# Physical pin 20
+GPIO4  = 2  # Physical pin 13
+GPIO5  = 3  # physical pin 17
+GPIO6  = 4  # Physcial pin 18
+GPIO7  = 6  # Physcial pin 26
+GPIO8  = 22 # Physcial pin 19
+GPIO9  = 27 # Physical pin 15
 
 def initPins():	
 	wiringPi.wiringPiSetup();
