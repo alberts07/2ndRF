c08a5d857f712a8713d77bf45d94de2e731297f8
Austin Alberts <aual4352@colorado.edu>
2017-03-16 10:46:38 -0600

    Updating Error codes

--- odroid/yfcalc.py
+++ odroid/yfcalc.py
@@ -13,29 +13,30 @@ from datetime import datetime
 
 def NoiseFig(N2,N1):
 
-	try:
-		ENR=14.85
-		YF=N2/N1
-		NF= ENR-10*math.log(YF-1,10)
-		return (NF, str(datetime.now()))
-	except TypeError:
-		print("You need to enter a number")
+    try:
+        ENR=14.85
+        YF=N2/N1
+        NF= ENR-10*math.log(YF-1,10)
+        return (NF, str(datetime.now()))
+    except TypeError:
+        print("You need to enter a number")
 def filewrite(data):
-	path = '/home/sensor/data_archive/'
+    path = '/home/sensor/data_archive/'
+    finalpath ='NoiseFigure.xls'
 
-	try:
-		rb = open_workbook("NoiseFigure.xls", formatting_info=True)
-		r_sheet = rb.sheet_by_index(0)  # read only copy to introspect the file
-		wb = copy(rb)  # a writable copy (I can't read values out of this, only write to it)
-		w_sheet = wb.get_sheet(0)  # the sheet to write to within the writable copy
-		row = r_sheet.nrows+1
-		for i in range(len(data)):
-			w_sheet.write(row-1, i, data[i])
-		print(path+"NoiseFigure.xls")
-		wb.save("NoiseFigure.xls")
-		print("Successfully wrote %f and %s" %(data[0], data[1]))
-	except:
-		print("Failed to write the NoiseFigure")
+    try:
+        rb = open_workbook(finalpath, formatting_info=True)
+        r_sheet = rb.sheet_by_index(0)  # read only copy to introspect the file
+        wb = copy(rb)  # a writable copy (I can't read values out of this, only write to it)
+        w_sheet = wb.get_sheet(0)  # the sheet to write to within the writable copy
+        row = r_sheet.nrows+1
+        for i in range(len(data)):
+            w_sheet.write(row-1, i, data[i])
+        print(finalpath)
+        wb.save(finalpath)
+        print("Successfully wrote %f and %s" %(data[0], data[1]))
+    except IOError:
+        print("The file name, %s, is not valid" %finalpath)
 
 data=NoiseFig(795.02,7.9499)
 filewrite(data)
