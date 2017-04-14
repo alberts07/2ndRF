70e06bde371f3622663991bd7b5b88966009c6f2
jayhuang520 <kingsleyzhijiehuang@gmail.com>
2017-04-14 02:45:10 +0000

    Update RX

--- receive/top_block.py
+++ receive/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Wed Apr 12 18:55:37 2017
+# Generated: Fri Apr 14 02:44:02 2017
 ##################################################
 
 if __name__ == '__main__':
@@ -23,12 +23,14 @@ from gnuradio import eng_notation
 from gnuradio import filter
 from gnuradio import gr
 from gnuradio import qtgui
+from gnuradio import uhd
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
 import sip
 import sys
+import time
 
 
 class top_block(gr.top_block, Qt.QWidget):
@@ -63,20 +65,33 @@ class top_block(gr.top_block, Qt.QWidget):
         self.gain = gain = 0
         self.ft = ft = 75E3
         self.fm = fm = 1000
+        self.fc_fine = fc_fine = 0
         self.fc = fc = 3.625E9
 
         ##################################################
         # Blocks
         ##################################################
-        self.low_pass_filter_0_1 = filter.fir_filter_fff(1, firdes.low_pass(
-        	1, samp_rate, fm*4, fm/2, firdes.WIN_HAMMING, 6.76))
         self._gain_range = Range(0, 30, 1, 0, 200)
         self._gain_win = RangeWidget(self._gain_range, self.set_gain, "gain", "counter_slider", float)
-        self.top_grid_layout.addWidget(self._gain_win, 4,0,1,1)
-        self.blocks_multiply_xx_0_0 = blocks.multiply_vcc(1)
-        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
-        self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, ft, 1, 0)
-        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, fm, 1, 0)
+        self.top_grid_layout.addWidget(self._gain_win, 0,0,1,1)
+        self._fc_fine_range = Range(-1E9, 1E9, 10E6, 0, 200)
+        self._fc_fine_win = RangeWidget(self._fc_fine_range, self.set_fc_fine, "fc_fine", "counter_slider", float)
+        self.top_grid_layout.addWidget(self._fc_fine_win, 0,1,1,1)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+        	",".join(("", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(fc, 0)
+        self.uhd_usrp_source_0.set_gain(gain, 0)
+        self.uhd_usrp_source_0.set_antenna("TX/RX", 0)
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, samp_rate, fm*4, fm/2, firdes.WIN_HAMMING, 6.76))
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, fc_fine, 1, 0)
         self.Time_RX = qtgui.time_sink_c(
         	1024, #size
         	samp_rate, #samp_rate
@@ -125,7 +140,7 @@ class top_block(gr.top_block, Qt.QWidget):
             self.Time_RX.set_line_alpha(i, alphas[i])
         
         self._Time_RX_win = sip.wrapinstance(self.Time_RX.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._Time_RX_win, 3,0,1,1)
+        self.top_grid_layout.addWidget(self._Time_RX_win, 1,0,1,1)
         self.Freq_RX = qtgui.freq_sink_c(
         	1024, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
@@ -166,18 +181,16 @@ class top_block(gr.top_block, Qt.QWidget):
             self.Freq_RX.set_line_alpha(i, alphas[i])
         
         self._Freq_RX_win = sip.wrapinstance(self.Freq_RX.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._Freq_RX_win, 2,0,1,1)
+        self.top_grid_layout.addWidget(self._Freq_RX_win, 1,1,1,1)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_float_to_complex_0, 1))    
-        self.connect((self.analog_sig_source_x_0_0, 0), (self.low_pass_filter_0_1, 0))    
-        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_multiply_xx_0_0, 1))    
-        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_multiply_xx_0_0, 0))    
-        self.connect((self.blocks_multiply_xx_0_0, 0), (self.Freq_RX, 0))    
-        self.connect((self.blocks_multiply_xx_0_0, 0), (self.Time_RX, 0))    
-        self.connect((self.low_pass_filter_0_1, 0), (self.blocks_float_to_complex_0, 0))    
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))    
+        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.Freq_RX, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.Time_RX, 0))    
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_multiply_xx_0, 0))    
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
@@ -190,38 +203,46 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.Freq_RX.set_frequency_range(0, self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.fm*4, self.fm/2, firdes.WIN_HAMMING, 6.76))
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
         self.Time_RX.set_samp_rate(self.samp_rate)
-        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
-        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
-        self.low_pass_filter_0_1.set_taps(firdes.low_pass(1, self.samp_rate, self.fm*4, self.fm/2, firdes.WIN_HAMMING, 6.76))
+        self.Freq_RX.set_frequency_range(0, self.samp_rate)
 
     def get_gain(self):
         return self.gain
 
     def set_gain(self, gain):
         self.gain = gain
+        self.uhd_usrp_source_0.set_gain(self.gain, 0)
+        	
 
     def get_ft(self):
         return self.ft
 
     def set_ft(self, ft):
         self.ft = ft
-        self.analog_sig_source_x_1.set_frequency(self.ft)
 
     def get_fm(self):
         return self.fm
 
     def set_fm(self, fm):
         self.fm = fm
-        self.analog_sig_source_x_0_0.set_frequency(self.fm)
-        self.low_pass_filter_0_1.set_taps(firdes.low_pass(1, self.samp_rate, self.fm*4, self.fm/2, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.fm*4, self.fm/2, firdes.WIN_HAMMING, 6.76))
+
+    def get_fc_fine(self):
+        return self.fc_fine
+
+    def set_fc_fine(self, fc_fine):
+        self.fc_fine = fc_fine
+        self.analog_sig_source_x_0.set_frequency(self.fc_fine)
 
     def get_fc(self):
         return self.fc
 
     def set_fc(self, fc):
         self.fc = fc
+        self.uhd_usrp_source_0.set_center_freq(self.fc, 0)
 
 
 def main(top_block_cls=top_block, options=None):
