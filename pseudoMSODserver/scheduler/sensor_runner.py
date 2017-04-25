7372240dd6b77073ac20e5ddea4e4df1385fcb44
brdo5692 <brdo5692@colorado.edu>
2017-04-25 10:02:40 -0600

    bland scheduler done

--- pseudoMSODserver/scheduler/sensor_runner.py
+++ pseudoMSODserver/scheduler/sensor_runner.py
@@ -1,13 +1,42 @@
 import time
-from threading import Timer
+#from threading import Timer
+
+
 def print_time():
     print "From print_time", time.time()
 
+def receiver():
+    print("I'm in the receiver function now")
+    print("waiting for 4 seconds")
+    time.sleep(4)
+    print("Returning from receiver")
+
+def calculabrate():
+    print("Now I'm in the calculabrating function now")
+    print("waiting for 3 seconds")
+    time.sleep(3)
+    print("Returning from receiver")
+
+
+calcTime = 15
+
+if __name__ == "__main__":
+
+    tic = time.time()
+    while True:
+        receiver()
+
+        elapsedTime = time.time()-tic
+        if elapsedTime >= calcTime:
+            calculabrate()
+            tic = time.time()
+
+
+"""
 def print_some_times():
     print time.time()
     Timer(5, print_time, ()).start()
     Timer(10, print_time, ()).start()
     time.sleep(11)  # sleep while time-delay events execute
     print time.time()
-
-print_some_times()
+"""
